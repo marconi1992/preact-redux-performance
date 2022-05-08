4c4ff3cae17a6aef5e529963975d542445899ba8
Felipe Guizar <fguizar@etsy.com>
2022-05-07 23:51:21 -0500

    update docs

--- src/routes/wasted-renders/index.jsx
+++ src/routes/wasted-renders/index.jsx
@@ -1,12 +1,11 @@
 const WastedRenders = () => {
     return (
-        <div className="container-text mx-auto pt-6">
+        <div className="container-text mx-auto pt-6 pb-6">
             <section className="mb-6">
                 <h1 className="text-4xl mb-3">What is a wasted render?</h1>
-                <p className="text-lg mb-2">In React, the rendering process consists of getting the elements (output) from a component by calling the "render" function 
-                for a class-based component or the function itself for a functional component</p>
-                <p className="text-lg mb-2">So, "wasted renders" happen when the component renders again but the output is the same,
-                then React spends resources calling the render functions and comparing (diffing) the Virtual DOM changes.</p>
+                <p className="text-lg">The rendering process consists of getting the elements (output) from a component by calling the "render" function.</p>
+                <img src="/assets/example.png" alt="example component" />
+                <p className="text-lg mb-2">A <strong>"wasted render"</strong> would happen when the component renders again but the output is the same.</p>
             </section>
             <section className="mb-6">
                 <h1 className="text-4xl mb-3">When a component renders again?</h1>
