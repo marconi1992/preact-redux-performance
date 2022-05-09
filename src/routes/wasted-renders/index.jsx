4581b35871677e06a4916e8dcb2a953b7878c319
Felipe Guizar <fguizar@etsy.com>
2022-05-09 01:21:40 -0500

    update wasted render section

--- src/routes/wasted-renders/index.jsx
+++ src/routes/wasted-renders/index.jsx
@@ -3,9 +3,8 @@ const WastedRenders = () => {
         <div className="container-text mx-auto pt-6 pb-6">
             <section className="mb-6">
                 <h1 className="text-4xl mb-3">What is a wasted render?</h1>
-                <p className="text-lg">The rendering process consists of getting the elements (output) from a component by calling the "render" function.</p>
-                <img className="pt-6 pb-6" src="/assets/wasted-render.png" alt="example component" />
                 <p className="text-lg mb-2">A <strong>"wasted render"</strong> would happen when the component renders again but the output is the same.</p>
+                <img className="pt-6 pb-6" src="/assets/wasted-render.png" alt="example component" />
             </section>
             <section className="mb-6">
                 <h1 className="text-3xl mb-3">When a component renders again?</h1>
